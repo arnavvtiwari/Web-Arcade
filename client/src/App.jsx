0a3c59cca1f8fc5764248107cc29711b853913d3
Arnav Tiwari <arnav.luck@gmail.com>
2024-10-16 12:40:29 +0530

    bugs App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from './components/header/Header'
 import { Outlet } from 'react-router-dom'
-import Footer from './components/footer/footer'
+import Footer from './components/footer/Footer'
 
 const App = () => {
   return (
